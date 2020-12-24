f3a6e0205af2919946c96ef37bfa98bfb4dec290
shizhaoheng <mi@123456>
2020-12-24 11:51:14 +0800

    mybatis环境搭建

---
+++ src/test/java/com/itheima/test/MybatisTest.java
@@ -0,0 +1,19 @@
+package com.itheima.test;
+
+/**
+ * @Author : shi zhao heng
+ * @Date :   2020/12/23 21:48
+ * @Description :
+ * @Modified :
+ */
+public class MybatisTest {
+    /**
+     * 入门案例
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("test");
+
+    }
+}
