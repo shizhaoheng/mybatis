be62b021c927696ca9b3f3b3e2b4db9d25d183d3
shizhaoheng <mi@123456>
2021-01-07 16:46:08 +0800

    动态sql练习

--- src/main/java/com/itheima/domain/QueryVo.java
+++ src/main/java/com/itheima/domain/QueryVo.java
@@ -1,5 +1,7 @@
 package com.itheima.domain;
 
+import java.util.List;
+
 /**
  * @Author : shi zhao heng
  * @Date :   2021/1/4 17:10
@@ -7,6 +9,7 @@ package com.itheima.domain;
  */
 public class QueryVo {
     private User user;
+    private List<Integer> ids;
 
     public User getUser() {
         return user;
@@ -14,5 +17,13 @@ public class QueryVo {
 
     public void setUser(User user) {
         this.user = user;
+    }
+
+    public List<Integer> getIds() {
+        return ids;
+    }
+
+    public void setIds(List<Integer> ids) {
+        this.ids = ids;
     }
 }
--- src/test/java/com/itheima/test/MybatisTest.java
+++ src/test/java/com/itheima/test/MybatisTest.java
@@ -12,6 +12,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -180,5 +181,45 @@ public class MybatisTest {
     }
 
 
+    /**
+     * 测试查询所有
+     */
+    @Test
+    public void testFindByCondition(){
+        User u = new User();
+        u.setUserName("老王");
+        u.setUserSex("女");
+
+        //5.执行查询所有方法
+        List<User> users = userDao.findUserByCondition(u);
+        for(User user : users){
+            System.out.println(user);
+        }
+
     }
+
+
+    /**
+     * 测试foreach标签的使用
+     */
+    @Test
+    public void testFindInIds(){
+        QueryVo vo = new QueryVo();
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(41);
+        list.add(42);
+        list.add(46);
+        vo.setIds(list);
+
+
+        //5.执行查询所有方法
+        List<User> users = userDao.findUserInIds(vo);
+        for(User user : users){
+            System.out.println(user);
+        }
+
+    }
+
+
+}
 
